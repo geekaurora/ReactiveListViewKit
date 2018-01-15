3d8efa39e36ceb67d0e51c7b4208335e56f1e84c
Cheng Zhang <showt2me@gmail.com>
2018-01-15 12:29:07 -0800

    Update user profile pics

--- Example/ReactiveListViewKitDemo/Data Layer/CZMocker.swift
+++ Example/ReactiveListViewKitDemo/Data Layer/CZMocker.swift
@@ -31,16 +31,16 @@ class CZMocker: NSObject {
     /// Mocked hotUsers
     var hotUsers: [User] {
         let portraitUrls = [
-            "https://scontent-sjc2-1.cdninstagram.com/t51.2885-19/s150x150/15876365_735808813238573_5209783394333884416_n.jpg",
-            "https://scontent-sjc2-1.cdninstagram.com/t51.2885-19/s150x150/17932205_1873589956192972_4070576927188975616_a.jpg",
-            "https://scontent-sjc2-1.cdninstagram.com/t51.2885-19/11848925_1533801120173991_2075936696_a.jpg",
-            "https://scontent-sjc2-1.cdninstagram.com/t51.2885-19/s320x320/12976159_1725524527701278_1667923884_a.jpg",
-            "https://scontent-sjc2-1.cdninstagram.com/t51.2885-19/s320x320/12346207_957997364275563_865945097_a.jpg",
-            "https://scontent-sjc2-1.cdninstagram.com/t51.2885-19/10986348_437266659769074_1534648574_a.jpg",
-            "https://scontent-sjc2-1.cdninstagram.com/t51.2885-19/s150x150/20766151_129416397674687_1011941475153346560_n.jpg",
-            "https://scontent-sjc2-1.cdninstagram.com/t51.2885-19/s320x320/12093737_1642531946014435_1468116990_a.jpg",
-            "https://scontent-sjc2-1.cdninstagram.com/t51.2885-19/17495183_276607699428941_346402392461279232_a.jpg",
-            "https://scontent-sjc2-1.cdninstagram.com/t51.2885-19/s320x320/14733622_204530439986130_324838435409887232_a.jpg",
+            "https://scontent.cdninstagram.com/t51.2885-19/s150x150/15876365_735808813238573_5209783394333884416_n.jpg",
+            "https://scontent.cdninstagram.com/t51.2885-19/s150x150/17932205_1873589956192972_4070576927188975616_a.jpg",
+            "https://scontent.cdninstagram.com/t51.2885-19/11848925_1533801120173991_2075936696_a.jpg",
+            "https://scontent.cdninstagram.com/t51.2885-19/s320x320/12976159_1725524527701278_1667923884_a.jpg",
+            "https://scontent.cdninstagram.com/t51.2885-19/s320x320/12346207_957997364275563_865945097_a.jpg",
+            "https://scontent.cdninstagram.com/t51.2885-19/10986348_437266659769074_1534648574_a.jpg",
+            "https://scontent.cdninstagram.com/t51.2885-19/s150x150/20766151_129416397674687_1011941475153346560_n.jpg",
+            "https://scontent.cdninstagram.com/t51.2885-19/s320x320/12093737_1642531946014435_1468116990_a.jpg",
+            "https://scontent.cdninstagram.com/t51.2885-19/17495183_276607699428941_346402392461279232_a.jpg",
+            "https://scontent.cdninstagram.com/t51.2885-19/s320x320/14733622_204530439986130_324838435409887232_a.jpg",
             ]
         let userNameTuples = [
             ("fallinlight","Diane"),
